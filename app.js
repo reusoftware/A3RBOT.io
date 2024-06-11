0f65231509470a2bac6dce672b0ed95a2fa89374
reusoftware <105401707+reusoftware@users.noreply.github.com>
2024-06-11 18:06:03 +0800

    index.html

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ABOT WEB ChatBOT</title>
+    <title>A3R WEB ChatBOT</title>
     <style>
         body {
             font-family: Arial, sans-serif;
