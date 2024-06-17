97f05a4b2a0a0f9da5b5fd73337ec540d8ee365e
reusoftware <105401707+reusoftware@users.noreply.github.com>
2024-06-17 17:43:00 +0800

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-   document.addEventListener("DOMContentLoaded", function() {
+ document.addEventListener("DOMContentLoaded", function() { 
  let socket;
     let isConnected = false;
     let packetIdNum = 0;
